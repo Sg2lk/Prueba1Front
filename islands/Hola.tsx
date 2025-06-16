fc95fb1f33e2405b0d872012150a031196cacd5c
DolceCola <alexander10.apr@gmail.com>
2025-06-16 20:46:28 +0200

    update 1

---
+++ islands/Hola.tsx
@@ -0,0 +1,11 @@
+import { FunctionalComponent } from "preact/src/index.d.ts";
+
+const Hola:FunctionalComponent = () => {
+    return(
+        <>
+        Holass
+        </>
+    )
+}
+
+export default Hola;
---
+++ routes/_layout.tsx
@@ -0,0 +1,10 @@
+import { FreshContext } from "$fresh/src/server/mod.ts";
+
+export default async function handler(req: Request, c: FreshContext) {
+    return(
+        <div>
+            Soy layout, 
+            <c.Component/>
+        </div>
+    )
+}
